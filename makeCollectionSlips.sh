e1cbdb95c40468cfa8827599f85986b646a1b4d7
Tom Downes <tomwod@gmail.com>
2015-07-17 21:04:47 +0100

    fix the shell script

--- makeCollectionSlips.sh
+++ makeCollectionSlips.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-XMLFILE=/tmp/dump.xml
-CVSCRIPT=/home/tdownes/workspace/foodAssembly/fromXml.py
+
+# this dir..
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# temp dir to make the intermediate xml from the pdf
+TMP=$(dirname $(mktemp tmp.XXXXXXXXXX -ut))
+
+XMLFILE=$TMP/dump.xml
+CVSCRIPT=$DIR/fromXml.py
+
 echo Converting pdf to xml
 pdftohtml -enc ISO-8859-6 -i -s -wbt 20 -xml $1 $XMLFILE
 
 echo running $CVSCRIPT $XMLFILE
-python $CVSCRIPT $XMLFILE  > "${1%.pdf}.html"
+python $CVSCRIPT $XMLFILE  > "${DIR}/${1%.pdf}.html"
